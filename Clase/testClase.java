45bed534274b89e40bfb46f761911ead3b3d0a78
FerminBlanco <87425664+FerminBlancoRamirez@users.noreply.github.com>
2025-10-13 12:48:02 +0200

    Add files via upload

---
+++ Clase/testClase.java
@@ -0,0 +1,15 @@
+package Clase;
+
+public class testClase {
+    static void main() {
+        Clase c1=new Clase();
+        c1.setNumAlumnos();
+        c1.setChicos();
+        c1.setChicas();
+        c1.setPortatiles();
+        c1.setOrdenadores();
+        c1.setAñoActual();
+        c1.Descripcion();
+
+    }
+}
