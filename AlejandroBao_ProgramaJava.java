5f783c95147d4d50c0ed3395da8eaa2dcf1f3c1f
alejandrobao <alexbao090906@gmail.com>
2025-10-24 11:34:44 +0200

    Añado un programa sencillo de java

---
+++ AlejandroBao_ProgramaJava.java
@@ -0,0 +1,15 @@
+public class programa{
+
+    import java.util.Scanner;
+
+    public static void main (String[] args) {
+
+    Scanner sc = new Scanner (System.in);
+
+    System.out.println("Dime tu edad: ");
+    int edad = sc.nextInt();
+
+    System.out.println("Tienes "+ edad +" "+"años.");
+
+    }
+}
